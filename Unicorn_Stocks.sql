895ba53c44968b63d3d303dae1c21cab25b89da5
Indium01blr <blr0425ha001@indium.tech>
2025-04-12 12:49:03 +0530

    Add files via upload
    Added Tables:
    stocks
    stockshist

---
+++ Unicorn_Stocks.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `sys`.`stocks` (
+  `id` INT NOT NULL,
+  `name` VARCHAR(45) NOT NULL,
+  `Created` datetime DEFAULT NULL,
+  `Updated` datetime DEFAULT NULL,
+  `Deleted` varchar(45) DEFAULT NULL
+  );
+
+
+
+
+
